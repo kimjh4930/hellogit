a8511f77867375869fdc3ff2e24b430bd36d87d2
Junha <jhkim@popcornsar.com>
2018-08-01 09:58:45 +0900

    first commit

---
+++ HelloRCP/src/hellorcp/handlers/PopupHandler.java
@@ -0,0 +1,18 @@
+package hellorcp.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class PopupHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("handler");
+		
+		return null;
+	}
+
+}
---
+++ OtherHello/src/handlers/OtherHelloHandler.java
@@ -0,0 +1,18 @@
+package handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class OtherHelloHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Other Hello");
+		
+		return null;
+	}
+
+}
